753118c2a6ef001b6d9a10649e2ca2c84853839f
Rahul Kejriwal <kejriwalrahul@outlook.com>
2017-03-14 15:48:04 +0530

    mod readme

--- Code/script.py
+++ Code/script.py
@@ -1,22 +1,35 @@
+"""
+	Program for running Slotted Aloha Simulation multiple timesand graphing results
+
+	Written by Rahul Kejriwal
+"""
+
 import subprocess
 import matplotlib.pyplot as plt
-from tqdm import tqdm 
+# from tqdm import tqdm 
 
 # Simulation params
-N = 1000
+N = 50
 P = [0.01, 0.02, 0.03, 0.05, 0.1]
 W = [2, 4]
 M = 100000
 
 # Obtain simulation results
 results = []
-for w in tqdm(W):
+for w in W:
 	curr_res = []
 
-	for p in tqdm(P):
-		trial_op = subprocess.check_output(['java','sender','-N', str(N), '-W', str(w), '-p', str(p), '-M', str(M), '-m', '-r', '1000000'])
-		curr_res.append(trial_op.split("\n"))
-		# print trial_op.split("\n")
+	for p in P:
+		ops = [0,0,0,0,0,0]
+		for i in range(5):
+			trial_op = subprocess.check_output(['java','sender','-N', str(N), '-W', str(w), '-p', str(p), '-M', str(M), '-m', '-r', '1000'])
+			tops = trial_op.split("\n")
+			for i in range(len(ops)):
+				ops[i] += float(tops[i]) 
+
+		ops = [op/5 for op in ops]
+		print ops
+		curr_res.append(ops)
 
 	results.append(curr_res)
 
@@ -26,4 +39,5 @@ for idx, w in enumerate(W):
 	y = [t[4] for t in results[idx]]
 	print x,y	
 	plt.plot(x,y)
+	plt.plot(x,y, 'ro')
 plt.show()
